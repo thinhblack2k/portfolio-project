ccf880a8ce3fb601bd463c87695148d0c9427ae5
thinhblack2k <thinh191204033@gmail.com>
2024-01-01 14:48:43 +0700

    feat: Component Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import Image from "next/image"
+import { Socials } from "@/constants";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
+            <div className="flex flex-row gap-3 items-center">
+                <Image 
+                    src="/horseLogo.jpg"
+                    alt="logo"
+                    width={40}
+                    height={40}
+                    className="w-full h-full object-contain rounded-full"
+                />
+                <h1 className="text-white text-[25px] font-semibold">
+                Thinhdh.com
+            </h1>
+            </div>
+            <div className="flex flex-row gap-5 mb-2">
+                {Socials.map((social) => (
+                    <Image
+                        key={social.name}
+                        src={social.src}
+                        alt={social.name}
+                        width={28}
+                        height={28}
+                    />
+                ))
+
+                }
+            </div>
         </div>
     )
 }
